caf218213c19efd2c3dc4df778ae6fba0443a106
Joo Aun Saw <jasaw@dius.com.au>
2017-08-15 13:42:19 +1000

    build: xz compress image

--- build.sh
+++ build.sh
@@ -46,6 +46,9 @@ elif [ "$target" == "mkrelease" ]; then
     $boarddir/mkimage.sh
     cp $outputdir/images/$osname-$board.img $basedir
     mv $basedir/$osname-$board.img  $basedir/$osname-$board-$osversion.img
+    rm -f $basedir/$osname-$board-$osversion.tar.xz
+	XZ_OPT=-9 tar cJf $basedir/$osname-$board-$osversion.tar.xz $osname-$board-$osversion.img
+    echo "your image is ready at $basedir/$osname-$board-$osversion.tar.xz"
     rm -f $basedir/$osname-$board-$osversion.img.gz
     $gzip $basedir/$osname-$board-$osversion.img
     echo "your image is ready at $basedir/$osname-$board-$osversion.img.gz"
