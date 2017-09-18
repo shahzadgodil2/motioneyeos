6ed54ca5ff397670971e985a17ec01782b38123b
Joo Aun Saw <jasaw@dius.com.au>
2017-09-18 15:54:06 +1000

    motion: updated

--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOTION_VERSION = ab9e800d5984f2907f00bebabc794d1dba9682ad
+MOTION_VERSION = 2a140fa31feafd960033ad292615a601a1f3faec
 MOTION_SITE = $(call github,motion-project,motion,$(MOTION_VERSION))
 MOTION_AUTORECONF = YES
 MOTION_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib \
